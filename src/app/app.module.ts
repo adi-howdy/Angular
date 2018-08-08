a8ff14ce80b589ebefd72379affd6a8abc691e9c
adi-howdy <adityakhatri123@gmail.com>
2018-08-07 20:42:21 -0700

    grid list card module and button module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import 'hammerjs';
 
 import { MenuComponent } from './menu/menu.component';
@@ -22,6 +25,9 @@ import { MenuComponent } from './menu/menu.component';
     MatListModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
     MatToolbarModule
   ],
   providers: [],
